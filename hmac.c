2687a52fc416d1cabaef2d97e161fff81e972053
ywanHK <39464944+ywanHK@users.noreply.github.com>
2020-04-23 21:28:25 +0200

    Add files via upload

---
+++ hmac.c
@@ -0,0 +1,52 @@
+#include "stdio.h"
+#include "string.h"
+#include "stdlib.h"
+#include "sha256.c"
+void output(unsigned char *in,char format,int len){
+	for(int i=0;i<len;i++)printf("%02X%c",in[i],format);
+	printf("\n");
+}
+void xor(unsigned char *dest,int dest_len,unsigned char *src1,unsigned char *src2){
+	for(int i=0;i<dest_len;i++){
+		dest[i] = src1[i]^src2[i];
+	}
+}
+void pack(unsigned char *dest,int dest_len,unsigned long src){
+	dest += dest_len;
+	dest[3] = (unsigned char)src;
+	dest[2] = (unsigned char)(src>>8);
+	dest[1] = (unsigned char)(src>>16);
+	dest[0] = (unsigned char)(src>>24);
+}
+void hmac_sha256(const unsigned char *data,size_t len,const unsigned char *key,int len_key,unsigned char *out){
+	int block_size = 64;
+	int hash_size = 32;
+	int key_size = block_size;
+	unsigned char buf[block_size];
+	unsigned char buf2[block_size];
+	//bzero(buf,block_size);
+	//bzero(buf2,block_size);
+	memset(buf,0x00,block_size);
+	memset(buf2,0x00,block_size);
+	if(len_key>block_size){
+		sha256(key,len_key,buf);
+		memcpy(buf2,buf,hash_size);
+	}
+	else{
+		memcpy(buf,key,len_key);
+		memcpy(buf2,key,len_key);
+	}
+	for(int i=0;i<key_size;i++){
+		*(buf+i)=*(buf+i)^0x5c;
+		*(buf2+i)=*(buf2+i)^0x36;
+	}
+	size_t hash_buf_size = key_size+(len<hash_size?hash_size:len);
+	unsigned char hash_buf[hash_buf_size];
+	memcpy(hash_buf,buf2,key_size);
+	memcpy(hash_buf+key_size,data,len);
+	unsigned char hash_out[hash_size];
+	sha256(hash_buf,key_size+len,hash_out);
+	memcpy(hash_buf,buf,key_size);
+	memcpy(hash_buf+key_size,hash_out,hash_size);
+	sha256(hash_buf,key_size+hash_size,out);
+}
---
+++ main.c
@@ -0,0 +1,69 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+#include "hmac.c"
+#define ROUND 12
+
+
+// block has to be 32 bytes long
+// pwd is text, block is binary data
+void encrypt(unsigned char *message,char *pwd,unsigned char *out){
+	unsigned char lhs[16]={0},rhs[16]={0};
+	unsigned char key[ROUND][16],tmp[32];
+	memcpy(lhs,message,16);
+	memcpy(rhs,message+16,16);
+	sha256(pwd,strlen(pwd),tmp);
+	memcpy(key[0],tmp,16);
+	for(int i=1;i<ROUND;i++){
+		sha256(key[i-1],16,tmp);
+		memcpy(key[i],tmp,16);
+	}
+	memset(tmp,0x00,32);
+	for(int i=0;i<ROUND;i++){
+		hmac_sha256(rhs,16,key[0],16,tmp);
+		xor(tmp,16,lhs,tmp);
+		memcpy(lhs,rhs,16);
+		memcpy(rhs,tmp,16);
+	}
+	memcpy(out+16,lhs,16);
+	memcpy(out,rhs,16);
+}
+
+void decrypt(unsigned char *message,char *pwd,unsigned char *out){
+	unsigned char lhs[16]={0},rhs[16]={0};
+	unsigned char key[ROUND][16],tmp[32];
+	memcpy(lhs,message,16);
+	memcpy(rhs,message+16,16);
+	sha256(pwd,strlen(pwd),tmp);
+	memcpy(key[0],tmp,16);
+	for(int i=1;i<ROUND;i++){
+		sha256(key[i-1],16,tmp);
+		memcpy(key[i],tmp,16);
+	}
+	memset(tmp,0x00,32);
+	for(int i=ROUND-1;i>=0;i--){
+		hmac_sha256(rhs,16,key[0],16,tmp);
+		xor(tmp,16,lhs,tmp);
+		memcpy(lhs,rhs,16);
+		memcpy(rhs,tmp,16);
+	}
+	memcpy(out,lhs,16);
+	memcpy(out+16,rhs,16);
+}
+
+
+
+int main(int argc,char *argv[]){
+	unsigned char plain1[32],cipher[32],plain2[32];
+	memset(plain1,0x41,32);
+	encrypt(plain1,"test",cipher);
+	decrypt(cipher,"test",plain2);
+
+
+	output(plain1,' ',32);
+	output(cipher,' ',32);
+	output(plain2,' ',32);//*/
+
+	return 0;
+}
